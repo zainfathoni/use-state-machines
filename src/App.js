c6ae3f02ea1eacbdf7aacc3ff5d3b82520cd16ee
Zain Fathoni <zain.fathoni@gmail.com>
2019-05-31 00:57:15 +0800

    Remove serviceWorker

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react'
 import './App.css'
 import { ReservationCalendar } from './ReservationCalendar'
 
-export function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Reservation Calendar</header>
-      <ReservationCalendar />
-    </div>
-  )
-}
+export const App = () => (
+  <div className="App">
+    <header className="App-header">Reservation Calendar</header>
+    <ReservationCalendar />
+  </div>
+)
