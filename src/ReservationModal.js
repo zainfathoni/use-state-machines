93a354fe700634c062989f7bf22ea86a38416212
Zain Fathoni <zain.fathoni@gmail.com>
2019-05-31 03:15:32 +0800

    Add ReservationCard for detail view

--- src/ReservationModal.js
+++ src/ReservationModal.js
@@ -1,14 +1,39 @@
-import { Modal } from 'antd'
-import React from 'react'
+import { Modal, Icon } from 'antd'
+import React, { useState } from 'react'
 import { ReservationList } from './ReservationList'
+import { ReservationCard } from './ReservationCard'
+import { getDateData } from './utils'
 
 export const ReservationModal = ({ date, ...props }) => {
+  const [index, setIndex] = useState(-1)
+  const data = getDateData(date)
   return (
-    <Modal centered title={date && date.format('DD MMM YYYY')} {...props}>
-      <ReservationList
-        date={date}
-        onActionClick={index => console.log(index)}
-      />
+    <Modal
+      centered
+      title={
+        <>
+          {index >= 0 && (
+            <>
+              <Icon
+                type="left-circle"
+                theme="twoTone"
+                style={{ fontSize: 20 }}
+                onClick={() => setIndex(-1)}
+              />
+              &nbsp;&nbsp;
+            </>
+          )}
+          {date && date.format('DD MMM YYYY')}
+        </>
+      }
+      afterClose={() => setIndex(-1)}
+      {...props}
+    >
+      {props.visible && index >= 0 ? (
+        <ReservationCard rsvn={data[index]} />
+      ) : (
+        <ReservationList date={date} onActionClick={index => setIndex(index)} />
+      )}
     </Modal>
   )
 }
