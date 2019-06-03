a07326da2092168335c501a86582a0a12a8f876e
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-04 06:43:15 +0800

    Introducing guards aliasing

--- src/reservationMachine.js
+++ src/reservationMachine.js
@@ -1,26 +1,34 @@
 import { Machine } from 'xstate'
 
-export const reservationMachine = Machine({
-  initial: 'invisible',
-  states: {
-    invisible: {
-      id: 'invisible',
-      on: {
-        CLICK_DATE: [
-          { cond: data => data && data.length === 0, target: 'visible.edit' },
-          { cond: data => data && data.length === 1, target: 'visible.view' },
-          { target: 'visible' }
-        ]
-      }
-    },
-    visible: {
-      on: { CLOSE: 'invisible' },
-      initial: 'list',
-      states: {
-        list: { on: { VIEW: 'view' } },
-        view: { on: { EDIT: 'edit' } },
-        edit: { on: { SUBMIT: '#invisible' } } // use state id to refer parent state
+export const reservationMachine = Machine(
+  {
+    initial: 'invisible',
+    states: {
+      invisible: {
+        id: 'invisible',
+        on: {
+          CLICK_DATE: [
+            { cond: 'empty', target: 'visible.edit' },
+            { cond: 'single', target: 'visible.view' },
+            { target: 'visible' }
+          ]
+        }
+      },
+      visible: {
+        on: { CLOSE: 'invisible' },
+        initial: 'list',
+        states: {
+          list: { on: { VIEW: 'view' } },
+          view: { on: { EDIT: 'edit' } },
+          edit: { on: { SUBMIT: '#invisible' } } // use state id to refer parent state
+        }
       }
     }
+  },
+  {
+    guards: {
+      empty: data => data && data.length === 0,
+      single: data => data && data.length === 1
+    }
   }
-})
+)
