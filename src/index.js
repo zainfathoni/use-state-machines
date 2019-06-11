cc04b08dfa5c08a597335dc4f30a055b93bcc03c
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-11 22:08:20 +0700

    Create stepped folder

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
 import './index.css'
-import { App } from './demo/App'
+import { App } from './demo-1/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
