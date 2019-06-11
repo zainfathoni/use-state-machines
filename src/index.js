d23cf60c1192bdbf632af0fd91deb3bfe6799ce6
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-11 23:29:27 +0700

    Add demo-3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
 import './index.css'
 import './App.css'
-import { App } from './demo-1/App'
+import { App } from './demo-3/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
