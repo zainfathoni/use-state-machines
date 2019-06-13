b2e000a9c9858acb90c3b243bbac296d36862037
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-14 00:32:51 +0800

    Fix demo-c machine

--- src/demo-c/machine.js
+++ src/demo-c/machine.js
@@ -1,56 +1,44 @@
 import { Machine } from 'xstate'
 
-export const modalMachine = Machine(
-  {
-    id: 'createView',
-    initial: 'invisible',
-    states: {
-      invisible: {
-        id: 'invisible',
-        on: {
-          OPEN: [
-            {
-              cond: 'empty',
-              target: 'visible.create'
-            },
-            {
-              cond: 'single',
-              target: 'visible.view'
-            },
-            { target: 'visible' }
-          ]
-        }
+export const modalMachine = Machine({
+  id: 'createView',
+  initial: 'invisible',
+  states: {
+    invisible: {
+      id: 'invisible',
+      on: {
+        OPEN: [
+          {
+            cond: (context, event) =>
+              !event.data || (event.data && event.data.length === 0),
+            target: 'visible.create'
+          },
+          { target: 'visible' }
+        ]
+      }
+    },
+    visible: {
+      on: {
+        CLOSE: 'invisible'
       },
-      visible: {
-        on: {
-          CLOSE: 'invisible'
+      initial: 'view',
+      states: {
+        view: {
+          on: {
+            EDIT: 'edit'
+          }
         },
-        initial: 'view',
-        states: {
-          view: {
-            on: {
-              EDIT: 'edit'
-            }
-          },
-          edit: {
-            on: {
-              SUBMIT: '#invisible'
-            }
-          },
-          create: {
-            on: {
-              SUBMIT: '#invisible'
-            }
+        edit: {
+          on: {
+            SUBMIT: '#invisible'
+          }
+        },
+        create: {
+          on: {
+            SUBMIT: '#invisible'
           }
         }
       }
     }
-  },
-  {
-    guards: {
-      empty: (context, event) =>
-        !event.data || (event.data && event.data.length === 0),
-      single: (context, event) => event.data && event.data.length === 1
-    }
   }
-)
+})
